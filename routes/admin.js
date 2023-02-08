a733c0cbe6532e3a58e7e5d5179228431ba120a9
ANJITH B <116331266+ANJITH-B@users.noreply.github.com>
2023-02-08 20:50:35 +0530

    Add files via upload

---
+++ routes/admin.js
@@ -0,0 +1,34 @@
+var express = require('express');
+const { helpers } = require('handlebars');
+const productHelpers = require('../helpers/product-helpers');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  productHelpers.getAllProducts().then((products)=>{
+    res.render('admin/view-products',{admin:true,products});
+  })
+});
+router.get('/add-product',function(req,res){
+  res.render('admin/add-product')
+})
+router.post('/add-product',(req,res)=>{
+  //console.log(req.body);
+  // console.log(req.files.image);
+
+  productHelpers.addProduct(req.body,(insertedId)=>{
+    let image=req.files.image
+    //console.log(insertedId);
+    image.mv('./public/product-images/'+insertedId+'.jpg',(err)=>{
+      if(!err){
+        res.render("admin/add-product");
+        console.log('product added')
+      }
+      else{
+        console.log(err);
+      }
+    })
+  })
+})
+
+module.exports = router;
---
+++ routes/users.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+const productHelpers = require('../helpers/product-helpers');
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  productHelpers.getAllProducts().then((products)=>{
+    res.render('user/view-products',{admin:false,products});
+  })
+});
+router.get('/login',(req,res)=>{
+  res.render('user/login')
+})
+
+module.exports = router;
